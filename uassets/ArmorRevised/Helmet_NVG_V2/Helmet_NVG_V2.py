74a974b39637c92141116cb1048f906e463191bb
jakub-kbs <39801439+jakub-kbs@users.noreply.github.com>
2023-04-06 20:59:25 +0200

    Experimental files
    Experimental changes, including LUT for NVGs.

--- uassets/ArmorRevised/Helmet_NVG_V2/Helmet_NVG_V2.py
+++ uassets/ArmorRevised/Helmet_NVG_V2/Helmet_NVG_V2.py
@@ -9,16 +9,20 @@ path_to_uasset = 'D:\\_Work\\Unreal Tools\\UAassetGUI\\UAssetGUI.exe'
 # For every item in the table.
 for item in data["Exports"][5]["Data"][3]["Value"]:
 
-    if item["Name"] == "BloomIntensity":
-        item["Value"] = 1
+    if item["Name"] == "GrainIntensity":
+        item["Value"] = 0.4
         continue
 
-    if item["Name"] == "BloomThreshold":
-        item["Value"] = 1
+    if item["Name"] == "BloomIntensity":
+        item["Value"] = 1.25
         continue
+
+    # if item["Name"] == "BloomThreshold":
+    #     item["Value"] = 1
+    #     continue
 
     if item["Name"] == "AutoExposureMinBrightness":
-        item["Value"] = -7.5
+        item["Value"] = -4
         continue
 
     if item["Name"] == "AutoExposureMaxBrightness":
@@ -26,23 +30,27 @@ for item in data["Exports"][5]["Data"][3]["Value"]:
         continue
 
     if item["Name"] == "AutoExposureSpeedUp":
-        item["Value"] = 7.5
+        item["Value"] = 2
         continue
 
     if item["Name"] == "AutoExposureSpeedDown":
-        item["Value"] = 7.5
+        item["Value"] = 2
+        continue
+
+    if item["Name"] == "IndirectLightingIntensity":
+        item["Value"] = 0.5
         continue
 
     if item["Name"] == "DepthOfFieldFocalDistance":
-        item["Value"] = 10000
+        item["Value"] = 100
         continue
 
     if item["Name"] == "DepthOfFieldDepthBlurAmount":
-        item["Value"] = 250
+        item["Value"] = 1000
         continue
 
     if item["Name"] == "DepthOfFieldDepthBlurRadius":
-        item["Value"] = 25
+        item["Value"] = 10
         continue
 
 #data["Exports"][5]["Data"][3]["Value"][66]["Value"] = 1
